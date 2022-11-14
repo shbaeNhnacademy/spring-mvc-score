0a491eb26c2fce52534c6631751bdb893f6d36c6
배수한 <qotngks152@naver.com>
2022-11-14 17:00:44 +0900

    입력 폼 검증 어노테이션 삽입

--- src/main/java/com/nhnacademy/score/domain/Student.java
+++ src/main/java/com/nhnacademy/score/domain/Student.java
@@ -1,14 +1,25 @@
 package com.nhnacademy.score.domain;
 
+import lombok.NonNull;
 import lombok.Value;
+
+import javax.validation.constraints.*;
 
 @Value
 public class Student {
     // TODO #1 valid 설정
+    @NonNull
     private long id;
+    @NotBlank
     private String name;
+    @Email
     private String email;
+    @Min(0)
+    @Max(100)
     private int score;
+    @NotBlank
+    @Size(max = 200)
     private String comment;
+
 
 }

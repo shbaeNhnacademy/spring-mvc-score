95a18f1cdeb4b4f4537a2ff9b85593346fa78553
배수한 <qotngks152@naver.com>
2022-11-17 18:04:02 +0900

    테스트 추가 겸 mofify.html 수정
    form쓸때는 post로 변경

--- src/test/java/com/nhnacademy/score/controller/StudentRestControllerTest.java
+++ src/test/java/com/nhnacademy/score/controller/StudentRestControllerTest.java
@@ -1,6 +1,8 @@
 package com.nhnacademy.score.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.score.domain.Student;
+import com.nhnacademy.score.domain.StudentModifyRequest;
 import com.nhnacademy.score.repository.StudentRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,12 +11,14 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
+import java.nio.charset.StandardCharsets;
+
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 class StudentRestControllerTest {
@@ -36,7 +40,8 @@ class StudentRestControllerTest {
         when(studentRepository.exists(anyLong())).thenReturn(true);
         when(studentRepository.getStudent(anyLong())).thenReturn(student);
         mockMvc.perform(get("/students/{studentId}", "1")
-                        .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").value("1"));
@@ -46,17 +51,87 @@ class StudentRestControllerTest {
     void viewStudent_differentStudentId_fail() throws Exception {
         Long id = 1L;
         Student student = Student.create(id, "", "", 0, "");
-        when(studentRepository.exists(anyLong())).thenReturn(true);
+        when(studentRepository.exists(anyLong())).thenReturn(false);
         when(studentRepository.getStudent(anyLong())).thenReturn(student);
         mockMvc.perform(get("/students/{studentId}", "2")
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value("1"));
+                .andExpect(status().isNotFound());
+        verify(studentRepository, times(1)).exists(anyLong());
+        verify(studentRepository, never()).getStudent(anyLong());
+
     }
 
     @Test
-    void modifyStudent() {
+    void modifyStudent_success() throws Exception {
+        long id =1L;
+        String name = "aaa";
+        String email = "2@1.com";
+        int score = 10;
+        String comment = "222";
+
+        Student preStudent = Student.create(1L, "", "", 0, "");
+        Student postStudent = Student.create(1L, name, email, score, comment);
+        StudentModifyRequest studentModifyRequest = new StudentModifyRequest();
+        studentModifyRequest.setName(name);
+        studentModifyRequest.setEmail(email);
+        studentModifyRequest.setScore(score);
+        studentModifyRequest.setComment(comment);
+
+        when(studentRepository.exists(anyLong())).thenReturn(true);
+        when(studentRepository.getStudent(anyLong())).thenReturn(preStudent);
+        when(studentRepository.modifyStudent(id, name, email, score, comment)).thenReturn(postStudent);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(studentModifyRequest);
+
+        mockMvc.perform(put("/students/{studentId}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value("1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(email))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.score").value(score))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.comment").value(comment));
+
+    }
+
+    @Test
+    void modifyStudent_invalidParam_fail() throws Exception {
+        long id =1L;
+        String name = "";
+        String email = "2@1";
+        int score = 1000;
+        String comment = "";
+
+        Student preStudent = Student.create(1L, "", "", 0, "");
+        Student postStudent = Student.create(1L, name, email, score, comment);
+        StudentModifyRequest studentModifyRequest = new StudentModifyRequest();
+        studentModifyRequest.setName(name);
+        studentModifyRequest.setEmail(email);
+        studentModifyRequest.setScore(score);
+        studentModifyRequest.setComment(comment);
+
+        when(studentRepository.exists(anyLong())).thenReturn(true);
+        when(studentRepository.getStudent(anyLong())).thenReturn(preStudent);
+        when(studentRepository.modifyStudent(id, name, email, score, comment)).thenReturn(postStudent);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(studentModifyRequest);
+
+        mockMvc.perform(put("/students/{studentId}", id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value("1"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value(name))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.email").value(email))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.score").value(score))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.comment").value(comment));
+
     }
 
     @Test
